04a8a74a213cb6f05fc0b156b746aa1b3cb450fa
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-05-23 00:45:52 +0330

    Setting up the delete function

--- app.py
+++ app.py
@@ -35,5 +35,18 @@ def index():
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('index.html', tasks=tasks)
 
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was an issue deleting this task!'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
