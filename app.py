14f1b54eb3cdfb0094bd0ccdaf24b1e73be5a026
Milad Valizadeh <miladvalizadeh2003@gmail.com>
2024-05-23 01:28:11 +0330

    Setting up the update function

--- app.py
+++ app.py
@@ -48,5 +48,23 @@ def delete(id):
         return 'There was an issue deleting this task!'
 
 
+@app.route('/update/<int:id>', methods=['POST', 'GET'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+
+    if request.method == 'POST':
+        task.content = request.form['content']
+
+        try:
+            db.session.commit()
+            return redirect('/')
+            
+        except:
+            return 'There was an issue updating your task!'
+        
+    else:
+        return render_template('update.html', task=task)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
